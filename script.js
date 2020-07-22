8d3b4eb2993caff769141e0e0d3956600f1ff491
Cas <testing@owners-MacBook-Pro-2.local>
2020-07-22 18:10:33 -0500

    Completed studio w/ 1st bonus

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 // TODO: add code here
 window.addEventListener("load", function(){
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
-         response.json().then(function(json){
+        response.json().then(function(json) {
+            json.sort((a, b) => (a.hoursInSpace < b.hoursInSpace) ? 1 : -1)
         const astronauts = document.getElementById("container");
+
         for(astronaut of json){  
-           astronauts.innerHTML += `<fieldset class="astronaut">
-           <div class="bio">
-               <h3> ${astronaut.firstName} ${astronaut.lastName}</h3>
-               <ul>
-                   <li>Hours in space: ${astronaut.hoursInSpace}</li>
-                   <li>Active: ${astronaut.active}</li>
-                   <li>Skills: ${astronaut.skills}</li>
+            astronauts.innerHTML += 
+            `<fieldset class="astronaut">
+            <div class="bio">
+                <h3> ${astronaut.firstName} ${astronaut.lastName}</h3>
+                    <ul>
+                    <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                    <li>Active: ${astronaut.active}</li>
+                    <li>Skills: ${astronaut.skills}</li>
                </ul>
            </div>
            <img class="avatar" src=${astronaut.picture}>
